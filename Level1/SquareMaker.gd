2f1b2b485128b39a1f8cc44a6c34595b04fb9d4e
Erik Metz <erikmetz@Eriks-MacBook-Air.local>
2019-01-06 13:15:38 -0500

    you can upload that or whatever

--- Level1/SquareMaker.gd
+++ Level1/SquareMaker.gd
@@ -3,7 +3,7 @@ extends Node
 var player
 
 func _ready():
-	player = preload("res://Player.tscn")
+	player = preload("res://Objects/Player/Player.tscn")
 	
 func _process(delta):
 	if Input.is_action_pressed("ui_select"):
