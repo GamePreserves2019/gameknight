d439c64f3dc1e8ab0995f7eb77c2a3c390a92359
robby <robbyef98@gmail.com>
2019-01-06 13:49:47 -0500

    dab

--- Level1/PopupDialog.gd
+++ Level1/PopupDialog.gd
@@ -2,7 +2,13 @@ extends PopupDialog
 
 func _ready():
 	popup()
+
 	get_node("button").connect("pressed", self, "_on_button_pressed")
+	get_node("button").text= "fish"
+	var lab = Label.new()
+	add_child(lab)
+	lab.text="dog"
+	print("T")
 
 	
 func _on_button_pressed():
